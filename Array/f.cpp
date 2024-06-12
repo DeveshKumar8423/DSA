c3966ec98f757a9d974fb9ad63a3a1130211fb0b
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-06-12 22:49:14 +0530

    Reverse an Array L1

---
+++ Array/f.cpp
@@ -0,0 +1,14 @@
+//Reverse an array Level 1
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[10] = {1,2,3,4,5,6,7,8,9,0};
+
+    for(int i=9 ; i>=0 ; i--){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
