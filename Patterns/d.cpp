1c608ea4af1cd0ffab21d594edacd8f63be0f6fc
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-06-19 07:55:37 +0530

    Create d.cpp

---
+++ Patterns/d.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int r,c;
+    cin>>r>>c;
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+            cout<<j<<" ";;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
