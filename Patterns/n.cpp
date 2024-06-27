8bb774568b1f18b5dd9ce448b36eba4b63e2aa91
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-06-27 06:31:47 +0530

    Create n.cpp

---
+++ Patterns/n.cpp
@@ -0,0 +1,17 @@
+//hollow rectangle pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int rows, cols;
+    cin >> rows >> cols;
+    for (int i = 1; i <= rows; i++){
+        for (int j = 1; j <= cols; j++){
+            if (i == 1 || i == rows || j == 1 || j == cols)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
